d89d4715f0523d5ba0b01fdd205e9e85e17fd13a
JustAdel <adabbs2014@gmail.com>
2019-12-27 01:23:45 +0100

    update gui

--- Playground/src/gui/ScannerGUI.java
+++ Playground/src/gui/ScannerGUI.java
@@ -28,7 +28,7 @@ public class ScannerGUI extends JFrame {
 
 	private static final Font BUTTON_FONT = new Font(Font.DIALOG, Font.BOLD, 20);
 	private static final Font TITLE_LABEL_FONT = new Font(Font.MONOSPACED, Font.BOLD, 24);
-	
+
 	private final String DEFAULT_SAVE_FILE = "autosave.ser";
 
 	protected JLabel titleLabel = new JLabel("Scan App");
@@ -67,35 +67,33 @@ public class ScannerGUI extends JFrame {
 	}
 
 	protected void initLayout() {
-		
+
 		BorderLayout border = new BorderLayout();
 		Container contentPane = getContentPane();
 		contentPane.setLayout(border);
 
-		//Title
+		// Title
 		titleLabel.setHorizontalAlignment(JLabel.CENTER);
 		titleLabel.setVerticalAlignment(JLabel.CENTER);
 		contentPane.add(titleLabel, BorderLayout.NORTH);
-		
-		
-		//Body
-		
+
+		// Corps
+
 		JPanel bodyPanel = new JPanel();
 		GridLayout grid = new GridLayout(2, 1);
 		bodyPanel.setLayout(grid);
-		
-			//Before Scan:
+
 		JPanel intermediatePanel = new JPanel();
 		intermediatePanel.setLayout(new BorderLayout());
-		
-				//Buttons
+
+		// Buttons
 		buttonsPanel = new JPanel();
 		buttonsPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
 		buttonsPanel.add(addButton);
 		runButton.setEnabled(false);
 		buttonsPanel.add(runButton);
-		
-				//Selected elements
+
+		// Selected elements
 		entPanel = new EntriesPanel(analysisList, runButton);
 		entPanel.setSize(500, 250);
 		JScrollPane scrollPane = new JScrollPane(entPanel);
@@ -104,15 +102,15 @@ public class ScannerGUI extends JFrame {
 		intermediatePanel.add(scrollPane, BorderLayout.CENTER);
 		bodyPanel.add(intermediatePanel);
 
-			//Results component
+		// Results component
 		resultsPanel = new ResultsPanel(results);
 		bodyPanel.add(resultsPanel, BorderLayout.SOUTH);
-		
+
 		contentPane.add(bodyPanel, BorderLayout.CENTER);
 
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(600, 480);
-		//pack();
+		// pack();
 		setResizable(true);
 		setVisible(true);
 	}
@@ -120,7 +118,7 @@ public class ScannerGUI extends JFrame {
 	private class AddAction implements ActionListener {
 
 		/**
-		 *génère un JFileChooser
+		 * génère un JFileChooser
 		 */
 		@Override
 		public void actionPerformed(ActionEvent e) {
@@ -128,7 +126,8 @@ public class ScannerGUI extends JFrame {
 			JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
 			jfc.setDialogTitle("Choose a file or a directory to scan: ");
 			jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-			//Ouvre une boite de dialogue personnalisée, avec un bouton de validation spécial.
+			// Ouvre une boite de dialogue personnalisée, avec un bouton de validation
+			// spécial.
 			int returnValue = jfc.showDialog(null, "Select");
 			if (returnValue == JFileChooser.APPROVE_OPTION) {
 				analysisList.addToScanList(jfc.getSelectedFile());
@@ -139,7 +138,7 @@ public class ScannerGUI extends JFrame {
 
 	private class RunScanAction implements ActionListener {
 		/**
-		 *lance l'analyse
+		 * lance l'analyse
 		 */
 		@Override
 		public void actionPerformed(ActionEvent e) {
@@ -154,17 +153,17 @@ public class ScannerGUI extends JFrame {
 	 * Met à jour le composant graphique responsable de l'affichage des résultats.
 	 */
 	private void updateResults() {
-		//On sauvegarde les résultats dans un fichier binaire.
+		// On sauvegarde les résultats dans un fichier binaire.
 		results.serializationSave(DEFAULT_SAVE_FILE);
-		//On libère l'espace pour les éventuelles analyses futures.
+		// On libère l'espace pour les éventuelles analyses futures.
 		results.clear();
-		//On consulte le fichier de sauvegarde...
+		// On consulte le fichier de sauvegarde...
 		ResultSave tmp = new ResultSave();
 		tmp.serializationRead(DEFAULT_SAVE_FILE);
-		//pour afficher les résultats de l'analyse.
+		// pour afficher les résultats de l'analyse.
 		resultsPanel.update(tmp);
 	}
-	
+
 	public static void main(String[] args) {
 		new ScannerGUI("Scanner GUI");
 	}
